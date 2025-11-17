01b1b7d1c4c63fb07df0d9a46f4c309ed67254b4
Leo <leolou.90@gmail.com>
2025-11-16 18:35:02 -0700

    Create interfaces folder for repositories

---
+++ QuickNotes.DataAccess.EF/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using QuickNotes.DataAccess.EF.Models;
+using QuickNotesAPI.DTO.UserDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickNotes.DataAccess.EF.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsers();
+
+        Task<User> GetUserById(int id);
+
+        Task<User> GetUserByName(string username);
+
+        Task<User> CreateUser(UserDTO Email);
+
+        Task<User> UpdateUser(int id, string email, string passwordhashed);
+
+        Task DeleteUser(int id);
+    }
+}
