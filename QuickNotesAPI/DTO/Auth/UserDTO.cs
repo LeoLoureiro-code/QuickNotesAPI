3330d52406abcad9e9d8723d874da40e0c33ecdb
Leo <leolou.90@gmail.com>
2025-11-16 18:36:11 -0700

    Create DTO and Auth folders, and create user DTO

---
+++ QuickNotesAPI/DTO/Auth/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickNotesAPI.DTO.UserDTO
+{
+    public class UserDTO
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
