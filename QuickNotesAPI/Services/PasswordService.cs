972e73393d4466ce11fe69cae27be8f630b0d888
Leo <leolou.90@gmail.com>
2025-11-18 18:40:57 -0700

    Create a service folder and a password service to hash password

---
+++ QuickNotesAPI/Services/PasswordService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using QuickNotesAPI.Services.Interfaces;
+
+namespace QuickNotesAPI.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        private readonly PasswordHasher<object> _hasher = new PasswordHasher<object>();
+
+        public string HashPassword(string password)
+        {   
+             return _hasher.HashPassword(null, password);
+        }
+
+        public bool VerifyPassword(string hashedPassword, string providedPassword)
+        {
+            var result = _hasher.VerifyHashedPassword(null, hashedPassword, providedPassword);
+            return result == PasswordVerificationResult.Success;
+        }
+    }
+}
