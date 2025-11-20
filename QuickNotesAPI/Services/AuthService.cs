0b69874cbf3745f8e2130280eb9bc5ca7bd092a8
Leo <leolou.90@gmail.com>
2025-11-19 18:46:01 -0700

    Add authorization service to use in the user repository

---
+++ QuickNotesAPI/Services/AuthService.cs
@@ -0,0 +1,17 @@
+﻿using QuickNotesAPI.Services.Interfaces;
+
+namespace QuickNotesAPI.Services
+{
+    public class AuthService : IAuthService
+    {
+        public Task<(string AccessToken, string RefreshToken, int userId)> LoginAsync(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RevokeAndRefreshTokenAsync(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
