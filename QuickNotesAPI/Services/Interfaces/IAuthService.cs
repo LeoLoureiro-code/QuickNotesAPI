cf4dca6936b0567d4ed7f32becdeafe66f68f312
Leo <leolou.90@gmail.com>
2025-11-19 18:45:29 -0700

    Add interface for authorization service

---
+++ QuickNotesAPI/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿namespace QuickNotesAPI.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<(string AccessToken, string RefreshToken, int userId)> LoginAsync(string username, string password);
+
+
+        Task RevokeAndRefreshTokenAsync(string email, string password);
+
+    }
+}
