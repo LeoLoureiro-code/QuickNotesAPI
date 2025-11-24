f5d6137414b2fbec71b886f6300a13641255104e
Leo <leolou.90@gmail.com>
2025-11-23 18:32:18 -0700

    Change arguments to use model

--- QuickNotes.DataAccess.EF/Repositories/Interfaces/IUserRepository.cs
+++ QuickNotes.DataAccess.EF/Repositories/Interfaces/IUserRepository.cs
@@ -1,5 +1,4 @@
 ﻿using QuickNotes.DataAccess.EF.Models;
-using QuickNotesAPI.DTO.UserDTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,10 +15,11 @@ namespace QuickNotes.DataAccess.EF.Repositories.Interfaces
 
         Task<User> GetUserByName(string username);
 
-        Task<User> CreateUser(UserDTO Email);
+        Task<User> CreateUser(User Email);
 
-        Task<User> UpdateUser(int id, string email, string password, string role);
+        Task<User> UpdateUser(uint id, string email, string password, string role);
 
         Task DeleteUser(int id);
+
     }
 }
