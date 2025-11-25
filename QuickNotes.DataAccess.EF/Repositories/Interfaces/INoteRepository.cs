3670622a90d10b114ab0a93776917acc826e4ecf
Leo <leolou.90@gmail.com>
2025-11-24 18:57:46 -0700

    Add Notes repository interface

---
+++ QuickNotes.DataAccess.EF/Repositories/Interfaces/INoteRepository.cs
@@ -0,0 +1,22 @@
+﻿using QuickNotes.DataAccess.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickNotes.DataAccess.EF.Repositories.Interfaces
+{
+    public interface INoteRepository
+    {
+        Task<IEnumerable<Note>> GetAllNotesByUser();
+
+        Task<User> GetNoteById(int id);
+
+        Task<User> CreateNote(Note note);
+
+        Task<User> UpdateUser(uint id, string noteTitle, string noteContent);
+
+        Task DeleteNote(int id);
+    }
+}
