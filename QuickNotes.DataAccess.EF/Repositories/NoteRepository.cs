42a1ea9d3b388cfad0ae843b5e8d1cd4eb79ea44
Leo <leolou.90@gmail.com>
2025-11-24 18:58:03 -0700

    Implement esqueleton interface

---
+++ QuickNotes.DataAccess.EF/Repositories/NoteRepository.cs
@@ -0,0 +1,38 @@
+﻿using QuickNotes.DataAccess.EF.Models;
+using QuickNotes.DataAccess.EF.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickNotes.DataAccess.EF.Repositories
+{
+    public class NoteRepository : INoteRepository
+    {
+        public Task<User> CreateNote(Note note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteNote(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Note>> GetAllNotesByUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetNoteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateUser(uint id, string noteTitle, string noteContent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
