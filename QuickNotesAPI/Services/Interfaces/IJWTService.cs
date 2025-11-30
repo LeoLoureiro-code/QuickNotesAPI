de14a77d69cfe4669f63cfaa181d23853520f2a5
Leo <leolou.90@gmail.com>
2025-11-30 16:05:01 -0700

    Add GenerateRefreshToken method

---
+++ QuickNotesAPI/Services/Interfaces/IJWTService.cs
@@ -0,0 +1,9 @@
+﻿namespace QuickNotesAPI.Services.Interfaces
+{
+    public interface IJWTService
+    {
+        string GenerateToken(uint userId, string email, string role);
+
+        string GenerateRefreshToken(uint userId, string email, string role);
+    }
+}
